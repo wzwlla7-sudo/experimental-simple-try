82f7e515d215e8925c5704fa4a4be88eb473f819
9354699814593321 <wzwlla7@gmail.com>
2025-11-17 15:41:51 +0100

    Add files via upload

--- encrypt.C
+++ encrypt.C
@@ -18,14 +18,14 @@ int matinv(int *mat,int nded){
 }
 int matadd(int *mat1,int *mat2,int nto){
     int arr[4];
-    for (int jdu=0;jdu<3;jdu++){
+    for (int jdu=0;jdu<4;jdu++){
         arr[jdu]=mat1[jdu]+mat2[jdu];
     }
     return arr[nto];
 }
 int matsub(int *mat1,int *mat2,int nto){
     int arr[4];
-    for (int jdu=0;jdu<3;jdu++){
+    for (int jdu=0;jdu<4;jdu++){
         arr[jdu]=mat1[jdu]-mat2[jdu];
     }
     return arr[nto];
@@ -45,7 +45,7 @@ int encrypt(int data, int key,int valof){
     int k4=(key/4096)%16;
     int k5=(key/65536)%16;
     int k6=(key/1048576)%16;
-    int k7=(key/167772166)%16;
+    int k7=(key/16777216)%16;
     int k8=(key/268435456)%16;
     int mattoenc1[]={n1,n2,n3,n4};
     int mattoenc2[]={n5,n6,n7,n8};
@@ -54,19 +54,19 @@ int encrypt(int data, int key,int valof){
     //first stage
     int st1_1[4];
     int st1_2[4];
-    for (int ajkud=0;ajkud<3;ajkud++){
-        st1_1[matadd(mattoenc1,key1,ajkud)];
-        st1_2[maxmul(mattoenc2,key2,ajkud)];
+    for (int ajkud=0;ajkud<4;ajkud++){
+        st1_1[ajkud]=matadd(mattoenc1,key1,ajkud);
+        st1_2[ajkud]=maxmul(mattoenc2,key2,ajkud);
     }
     int st2_1[4];
     int st2_2[4];
-    for (int ajkud=0;ajkud<3;ajkud++){
-        st2_1[maxmul(st1_1,key2,ajkud)];
-        st2_2[matadd(st1_2,key1,ajkud)];
+    for (int ajkud=0;ajkud<4;ajkud++){
+        st2_1[ajkud]=maxmul(st1_1,key2,ajkud);
+        st2_2[ajkud]=matadd(st1_2,key1,ajkud);
     }
     int st3_1[4];
     int st3_2[4];
-    for (int ajkud=0;ajkud<3;ajkud++){
+    for (int ajkud=0;ajkud<4;ajkud++){
         st3_1[ajkud]=st2_1[ajkud]^key;
         st3_2[ajkud]=st2_2[ajkud]^key;
     }
@@ -91,7 +91,7 @@ int decrypt(int *mattoenc1, int *mattoenc2, int key){
     int k4=(key/4096)%16;
     int k5=(key/65536)%16;
     int k6=(key/1048576)%16;
-    int k7=(key/167772166)%16;
+    int k7=(key/16777216)%16;
     int k8=(key/268435456)%16;
     //int mattoenc1[4]=data1;
     //int mattoenc2[4]=data2;
@@ -102,9 +102,9 @@ int decrypt(int *mattoenc1, int *mattoenc2, int key){
     int st1_2[4];
     int st2_1[4];
     int st2_2[4];
-    for (int ajkd=0;ajkd<3;ajkd++){
-        st2_1[ajkd]=mattoenc1[ajkd]^key;
-        st2_2[ajkd]=mattoenc2[ajkd]^key;
+    for (int ajkd=0;ajkd<4;ajkd++){
+        st1_1[ajkd]=mattoenc1[ajkd]^key;
+        st1_2[ajkd]=mattoenc2[ajkd]^key;
     }
     int unkey1[5];
     int unkey2[5];
@@ -113,13 +113,13 @@ int decrypt(int *mattoenc1, int *mattoenc2, int key){
         unkey1[ajkud]=matinv(key1,ajkud);
         unkey2[ajkud]=matinv(key2,ajkud);
     }
-    for (int ajkd=0;ajkd<3;ajkd++){
+    for (int ajkd=0;ajkd<4;ajkd++){
         st2_1[ajkd]=(maxmul(st1_1,unkey2,ajkd))/unkey2[4];
         st2_2[ajkd]=matsub(st1_2,key1,ajkd);
     }
     int st3_1[4];
     int st3_2[4];
-    for (int ajkd=0;ajkd<3;ajkd++){
+    for (int ajkd=0;ajkd<4;ajkd++){
         st3_1[ajkd]=matsub(st2_1,key2,ajkd); 
         st3_2[ajkd]=(maxmul(st2_2,unkey1,ajkd))/unkey1[4];
     }
@@ -131,12 +131,12 @@ int main(){
     int naud2[]={7,2,9,3};
     int encrpted1[4];
     int encrpted2[4];
-    for(int ajkd=0;ajkd<3;ajkd++){
+    for(int ajkd=0;ajkd<4;ajkd++){
         encrpted1[ajkd]=encrypt(15,1351,ajkd);
         encrpted2[ajkd]=encrypt(15,1351,ajkd+4);
     }
-    int decr;
+    int decr=0;
     decr=decrypt(encrpted1,encrpted2,1351);
-    printf(" %d \n",decr);
+    printf(" the result is : %d \n",decr);
     return 1;
 }
